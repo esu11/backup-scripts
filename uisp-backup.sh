fe4224397f103df9dd67a2be7316cb2006c80f50
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-03-04 13:08:19 -0600

    Update uisp-backup.sh

--- uisp-backup.sh
+++ uisp-backup.sh
@@ -5,7 +5,7 @@ dow=$(date +'%a')
 month=$(date +'%m')
 day=$(date +'%d')
 year=$(date +'%Y')
-isodate=$year-$month-$day
+isodate=$(date +'%Y-%m-%d')
 latestbackup=/home/unms/data/unms-backups/backups/$(ls /home/unms/data/unms-backups/backups/ -Art | tail -n 1)
 
 # Daily backup operation
