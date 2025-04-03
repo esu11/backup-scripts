d5e43e0cefadac3ebdf3ae1b573f2a84178ba744
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-04-03 10:39:26 -0500

    Create paxton-backup.bat

---
+++ paxton-backup.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+REM Set variables
+SET DOW=%date:~0,3%
+SET MONTH=%date:~4,2%
+SET DAY=%date:~7,2%
+SET YEAR=%date:~10,4%
+SET ISODATE=%YEAR%-%MONTH%-%DAY
+SET RCLONE="C:\Program Files\rclone\rclone.exe"
+LATESTBACKUP="C:\Net2 Access Control\Backup"
+
+REM Daily backup operation
+%RCLONE% delete GDrive:/Latest
+%RCLONE% copy %LATESTBACKUP% GDrive:/Latest
+
+REM Weekly backup operation
+IF %DOW%==Sun (goto WEEKLY) ELSE (goto MONTHLY)
+:WEEKLY
+%RCLONE% copy %LATESTBACKUP% GDrive:/Weekly
+
+REM Monthly backup operation
+:MONTHLY
+IF %DAY%==01 (goto NEXT) ELSE (goto END)
+:NEXT
+%RCLONE% copy %LATESTBACKUP% GDrive:/Monthly
+%RCLONE% delete GDrive:/Weekly
+
+:END
