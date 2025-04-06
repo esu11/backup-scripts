acaa49ff5bd28a3562eded1a73cbe3c6f9d48c4a
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-04-06 07:40:00 -0500

    Create monitorcast-backup.bat

---
+++ monitorcast-backup.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+REM Set variables
+SET DOW=%date:~0,3%
+SET MONTH=%date:~4,2%
+SET DAY=%date:~7,2%
+SET YEAR=%date:~10,4%
+SET ISODATE=%date:~10,4%-%date:~4,2%-%date:~7,2%
+SET RCLONE="C:\Program Files\rclone\rclone.exe"
+SET LATESTBACKUP="C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL"
+
+REM Daily backup operation
+%RCLONE% delete GDrive:/Latest
+%RCLONE% copy %LATESTBACKUP% GDrive:/Latest
+
+REM Weekly backup operation
+IF %DOW%==Sun (goto WEEKLY) ELSE (goto MONTHLY)
+:WEEKLY
+%RCLONE% copy %LATESTBACKUP% GDrive:/Weekly/%date:~10,4%-%date:~4,2%-%date:~7,2%
+
+REM Monthly backup operation
+:MONTHLY
+IF %DAY%==01 (goto NEXT) ELSE (goto END)
+:NEXT
+%RCLONE% copy %LATESTBACKUP% GDrive:/Monthly/%date:~10,4%-%date:~4,2%-%date:~7,2%
+%RCLONE% delete GDrive:/Weekly
+
+:END
