c5fa9e84995e580ec940452170a78793ed30eb0e
Wade Gibson <86313939+wadegib@users.noreply.github.com>
2025-04-06 06:14:57 -0500

    Update print-backup.bat

--- print-backup.bat
+++ print-backup.bat
@@ -5,7 +5,7 @@ SET DOW=%date:~0,3%
 SET MONTH=%date:~4,2%
 SET DAY=%date:~7,2%
 SET YEAR=%date:~10,4%
-SET ISODATE=%YEAR%-%MONTH%-%DAY
+SET ISODATE=%YEAR%-%MONTH%-%DAY%
 SET RCLONE="C:\Program Files\rclone\rclone.exe"
 FOR /F "delims=|" %%I IN ('DIR "C:\Program Files\PaperCut MF\server\data\backups" /B /O:D') DO SET LATESTBACKUP="C:\Program Files\PaperCut MF\server\data\backups\%%I"
 
