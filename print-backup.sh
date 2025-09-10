1b621c137bc11ca82636df62d1aed4fee7b84332
aw11 <alex.wyatt@esu11.org>
2025-09-10 15:41:55 -0500

    Update print-backup.sh

--- print-backup.sh
+++ print-backup.sh
@@ -16,7 +16,7 @@ rm $backupdir/daily/ppd/*.tar.gz
 rm $backupdir/daily/papercut/*.zip
 # Create new Daily backups
 nice -n 19 tar zcvf $backupdir/daily/cups/cups_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /etc/cups
-nice -n 19 tar zcvf $backupdir/daily/cups/ppd_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /usr/share/ppd
+nice -n 19 tar zcvf $backupdir/daily/ppd/ppd_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /usr/share/ppd
 nice -n 19 cp $pcbackup $backupdir/daily/papercut/
 # Latest backups to Google Drive
 nice -n 19 rclone delete GDrive:/Latest
