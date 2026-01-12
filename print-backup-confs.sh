83d44838d3b10d7b00e1c9f6f3494118ba64b8f0
aw11 <alex.wyatt@esu11.org>
2026-01-12 14:16:01 -0600

    Update print-backup-confs.sh

--- print-backup-confs.sh
+++ print-backup-confs.sh
@@ -18,7 +18,7 @@ rm $backupdir/daily/papercut/*.zip
 nice -n 19 tar zcvf $backupdir/daily/cups/cups_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /etc/cups
 nice -n 19 tar zcvf $backupdir/daily/ppd/ppd_backup_$(date +'%Y-%m-%d_%H%M').tar.gz /usr/share/ppd
 nice -n 19 cp $pcbackup $backupdir/daily/papercut/
-nice -n 19 tar zcvf $backupdir/daily/papercut/conf_backups_$(date +'%Y-%m-%d_%H%M').tar.gz\
+nice -n 19 tar zcvf /mnt/backups/daily/papercut/conf_backups_$(date +'%Y-%m-%d_%H%M').tar.gz\
   /home/papercut/providers/print/linux-x64/print-provider.conf\
   /home/papercut/pc-mobility-print/data/config/cloudprint.conf.toml\
   /home/papercut/pc-mobility-print/data/config/dns.conf.toml\
@@ -26,6 +26,7 @@ nice -n 19 tar zcvf $backupdir/daily/papercut/conf_backups_$(date +'%Y-%m-%d_%H%
   /home/papercut/pc-mobility-print/data/config/papercut-server.conf\
   /home/papercut/pc-mobility-print/data/config/printer.conf.toml\
   /home/papercut/server/server.properties
+  
 # Latest backups to Google Drive
 nice -n 19 rclone delete GDrive:/Latest
 nice -n 19 rclone copy $backupdir/daily/ GDrive:/Latest
